62888571625558e6f89cacd823815d7758cdf409
Ioanna <dovgopolaii@gmail.com>
2025-11-01 15:53:07 +0100

    add styles

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,24 @@
-console.log("Form");
+const formEl=document.querySelector(".feedback-form");
+const formData=JSON.parse(localStorage.getItem("feedback-form-state"))||{ email: "", message: "" };
+formEl.addEventListener("input", onInput);
+formEl.elements.email.value=formData.email;
+formEl.elements.message.value=formData.message;
+
+function onInput(ev){
+    formData[ev.target.name]=ev.target.value.trim();
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+}
+formEl.addEventListener("submit", onSabmit);
+
+function onSabmit(ev){
+    ev.preventDefault();
+    const {email,message}=ev.target.elements;
+    if(email.value.trim()===""||message.value.trim()===""){
+        return alert("Fill please all fields");
+    }
+    console.log(formData);
+    localStorage.removeItem("feedback-form-state");
+    formEl.reset();
+    formData.email="";
+    formData.message="";
+}
